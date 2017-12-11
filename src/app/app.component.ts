ec71a88bbaf4b050545ba289dd97fed7cbb4c902
threxx <theresa.zobel@student.hpi.de>
2017-12-11 23:42:01 +0100

    Try to draw response bboxes on image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,7 @@ export class AppComponent implements OnInit {
   private boundingBoxes = [[]];
   private imgName = '';
   private imgData;
+  private detectedBoundingBoxes;
 
   ngOnInit() {
     $('#imageCanvas').mousedown(e => this.handleMouseDown(e));
@@ -34,15 +35,15 @@ export class AppComponent implements OnInit {
     const file = event.srcElement.files;
     const postData = { }; // Put your form data variable. This is only example.
     this._service.postWithFile('http://127.0.0.1:5000/', postData, file).then(result => {
-      console.log(result, 'result');
-      this.getImages();
+      this.detectedBoundingBoxes = result['bboxes'];
+      this.getImages(this.detectedBoundingBoxes);
     });
   }
 
-  private getImages() {
+  private getImages(detectedBoundingBoxes) {
     this.buildings = document.getElementById('imageImport')['files'];
     this.imgAmount = Object.keys(this.buildings).length;
-    this.processCanvas();
+    this.processCanvas(detectedBoundingBoxes);
     this.showUploader = false;
   }
 
@@ -54,11 +55,11 @@ export class AppComponent implements OnInit {
       this.msg = 'All images have been tagged. Upload more!';
       this.index = 0;
     } else {
-      this.processCanvas();
+      this.processCanvas(this.detectedBoundingBoxes);
     }
   }
 
-  private processCanvas() {
+  private processCanvas(detectedBoundingBoxes) {
     const reader = new FileReader();
 
     reader.onload = function (e) {
@@ -69,15 +70,52 @@ export class AppComponent implements OnInit {
         canvas.width = img.width;
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0);
+        ctx.beginPath();
+        ctx.rect(detectedBoundingBoxes[0][0], detectedBoundingBoxes[0][1],
+          detectedBoundingBoxes[0][2] - detectedBoundingBoxes[0][0],
+          detectedBoundingBoxes[0][3] - detectedBoundingBoxes[0][1]);
+        // ctx.rect(188, 50, 200, 100);
+        ctx.fillStyle = 'yellow';
+        ctx.fill();
+        ctx.lineWidth = 7;
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
       };
       img.src = e.target['result'];
     };
     this.imgName = this.buildings[this.index]['name'];
     reader.readAsDataURL(this.buildings[this.index]);
+    // this.drawBBoxes();
+  }
+
+  private drawBBoxes() {
+    const canvas: any = document.getElementById('imageCanvas');
+    const ctx = canvas.getContext('2d');
+    this.detectedBoundingBoxes.forEach(function(bbox) {
+      console.log(bbox[0]);
+      console.log(bbox[0][0], bbox[0][1], bbox[0][2], bbox[0][3]);
+      console.log(canvas, ctx);
+      ctx.strokeStyle = '#F00';
+      ctx.beginPath();
+      ctx.rect(bbox[0][0], bbox[0][1], bbox[0][2] - bbox[0][0], bbox[0][3] - bbox[0][1]);
+      ctx.stroke();
+      canvas.style.cursor = 'default';
+    });
+
+    const c: any = document.getElementById('imageCanvas');
+    const context = c.getContext('2d');
+
+    context.beginPath();
+    context.rect(188, 50, 200, 100);
+    context.fillStyle = 'yellow';
+    context.fill();
+    context.lineWidth = 7;
+    context.strokeStyle = 'black';
+    context.stroke();
   }
 
   private resetImage() {
-    this.processCanvas();
+    this.processCanvas(this.detectedBoundingBoxes);
   }
 
   private saveToCsv() {
@@ -122,7 +160,7 @@ export class AppComponent implements OnInit {
   private drawBoundingBox(ctx, canvas, startX, startY, endX, endY) {
     ctx.strokeStyle = '#F00';
     ctx.beginPath();
-    ctx.rect(startX, startY, endX - this.startX, endY - startY);
+    ctx.rect(startX, startY, endX - startX, endY - startY);
     this.boundingBoxes.push([this.startX, this.startY, endX, endY]);
     ctx.stroke();
     canvas.style.cursor = 'default';
